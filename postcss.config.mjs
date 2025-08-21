10edddf39365a1aea9154fcf8b2d0ebbb3b71eab
Can Rollas <canrollas@gmail.com>
2025-08-21 15:36:42 +0300

    The postcss config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,7 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-  },
-};
-
-export default config;
+// postcss.config.mjs
+export default {
+    plugins: {
+        '@tailwindcss/postcss': {},
+        autoprefixer: {},
+    },
+}
